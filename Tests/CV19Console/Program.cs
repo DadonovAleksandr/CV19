deaed577abb9420d7aed04b4aa80bbb8e36e6c60
DadonovAD <DadonovAD@tvv.transneft.ru>
2022-01-23 20:46:18 +0300

    Тестирование бизнес-логики

--- Tests/CV19Console/Program.cs
+++ Tests/CV19Console/Program.cs
@@ -28,11 +28,11 @@ namespace CV19Console
             using var data_stream = GetDataStream().Result;
             using var data_reader = new StreamReader(data_stream);
 
-            while(!data_reader.EndOfStream)
+            while (!data_reader.EndOfStream)
             {
                 var line = data_reader.ReadLine();
-                if (string.IsNullOrWhiteSpace(line) || line.Length < 100) continue;
-                yield return line;
+                if (string.IsNullOrWhiteSpace(line)) continue;
+                yield return line.Replace("Korea,", "Korea-").Replace("Bonaire,", "Bonaire-");
             }
 
         }
@@ -45,6 +45,22 @@ namespace CV19Console
             .ToArray();
 
 
+        private static IEnumerable<(string Country, string Province, int[] Counts)> GetData()
+        {
+            var lines = GetDataLines()
+                .Skip(1)
+                .Select(line => line.Split(','));
+            
+            foreach(var row in lines)
+            {
+                var province = row[0].Trim();
+                var country_name = row[1].Trim(' ', '"');
+                var counts = row.Skip(4).Select(int.Parse).ToArray();
+
+
+                yield return (country_name, province, counts);
+            }
+        }
 
 
         static void Main(string[] args)
@@ -61,8 +77,12 @@ namespace CV19Console
             //foreach(var data_line in GetDataLines())
             //    Console.WriteLine(data_line);
 
-            var dates = GetDates();
-            Console.WriteLine(string.Join("\r\n", dates));
+            //var dates = GetDates();
+            //Console.WriteLine(string.Join("\r\n", dates));
+
+            var russia_data = GetData().First(v => v.Country.Equals("Russia", StringComparison.OrdinalIgnoreCase));
+            Console.WriteLine(string.Join("\r\n", GetDates().Zip(russia_data.Counts, (date, count) => $"{date:dd:MM} - {count}")));
+
 
             Console.ReadLine();
         }
