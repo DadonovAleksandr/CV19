7a7c56f481dde796824b6eed7763e0d27b893f1c
DadonovAD <DadonovAD@tvv.transneft.ru>
2022-01-23 21:50:11 +0300

    Добавлем тестовые данные в модель- представления

--- CV19/ViewModels/MainWindowViewModel.cs
+++ CV19/ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using CV19.Infrastructure.Commands;
+using CV19.Models;
 using CV19.ViewModels.Base;
 using System;
 using System.Collections.Generic;
@@ -13,13 +14,35 @@ namespace CV19.ViewModels
     internal class MainWindowViewModel : ViewModel
     {
 
+        private IEnumerable<DataPoint> _TestDataPoints;
+        /// <summary>
+        /// Тестовый набор данных для визуализации грфиков
+        /// </summary>
+        public IEnumerable<DataPoint> TestDataPoints
+        {
+            get => _TestDataPoints;
+            set => Set(ref _TestDataPoints, value);
+        }
+
+
         public MainWindowViewModel()
         {
             #region Commands
             CloseApplicationCommand = new LambdaCommand(OnCloseApplicationCommandExecuted, CanCloseApplicationCommandExecute);
-            
+
             #endregion
-        
+
+            var data_points = new List<DataPoint>((int)(360 / 0.1));
+            for (var x = 0d; x <= 360; x+=0.1)
+            {
+                const double to_rad = Math.PI / 108;
+                var y = Math.Sin(x * to_rad);
+
+                data_points.Add(new DataPoint { XValue = x, YValue = y });
+
+            }
+
+            TestDataPoints = data_points;
         }
 
         #region Commands
@@ -43,6 +66,7 @@ namespace CV19.ViewModels
 
         #endregion
 
+        #region Window title
         private string _Title = "Анализ статистики CV19";
         /// <summary>
         /// Заголовок окна
@@ -61,7 +85,11 @@ namespace CV19.ViewModels
             set => Set(ref _Title, value);
         }
 
+        #endregion
 
+
+
+        #region Status
         private string _Status = @"Готов!";
         /// <summary>
         /// Статус программы
@@ -71,7 +99,7 @@ namespace CV19.ViewModels
             get => _Status;
             set => Set(ref _Status, value);
         }
-
+        #endregion
 
     }
 }
