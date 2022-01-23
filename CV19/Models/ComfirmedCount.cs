9bfeb16b2a3827f49836e7fa74e181b525cb4f92
DadonovAD <DadonovAD@tvv.transneft.ru>
2022-01-23 21:48:16 +0300

    Добавляем модели

---
+++ CV19/Models/ComfirmedCount.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+
+namespace CV19.Models
+{
+    internal struct ComfirmedCount
+    {
+        public DateTime Date { get; set; }
+        public int Count { get; set; }
+    }
+}
---
+++ CV19/Models/PlaceInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+
+
+namespace CV19.Models
+{
+    internal class PlaceInfo
+    {
+        public string Name { get; set; }
+
+        public Point Location { get; set; }
+
+        public IEnumerable<ComfirmedCount> Counts { get; set; }
+
+
+     }
+}
