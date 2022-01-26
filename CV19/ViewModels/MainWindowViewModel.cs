bc7507edf6c60a510e93b01e707669a706af9e5a
DadonovAD <DadonovAD@tvv.transneft.ru>
2022-01-26 12:01:00 +0300

    Добавлено свойство Выбранная группа в модель-представление

--- CV19/ViewModels/MainWindowViewModel.cs
+++ CV19/ViewModels/MainWindowViewModel.cs
@@ -18,6 +18,16 @@ namespace CV19.ViewModels
         /*--------------------------------------------------------------------------------*/
         public ObservableCollection<Group> Groups { get; }
 
+        #region Выбранная группа студентов
+        private Group _SelectedGroup;
+
+        public Group SelectedGroup
+        {
+            get => _SelectedGroup;
+            set => Set(ref _SelectedGroup, value);
+        }
+        #endregion
+
         #region SelectedPageIndex
         private int _SelectedPageIndex;
         /// <summary>
