ffc7805b7fc2ee4fe1933e238540fefa7990b501
DadonovAD <DadonovAD@tvv.transneft.ru>
2022-01-26 10:41:20 +0300

    Тестовая предметная область

---
+++ CV19/Models/Decanat/Student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace CV19.Models.Decanat
+{
+    internal class Student
+    {
+        public string Name { get; set; }
+        public string Suname { get; set; }
+        public string Patronymic { get; set; }
+        public DateTime Birthday { get; set; }
+        public double Rating{ get; set; }
+    }
+
+
+    internal class Group
+    {
+        public string Name { get; set; }
+
+        public ICollection<Student> Students { get; set; }
+    }
+}
