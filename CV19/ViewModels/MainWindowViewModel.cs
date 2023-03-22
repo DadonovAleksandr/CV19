207b7acbf547b04940544888bcca081f08e450cc
DadonovAD <DadonovAD@tvv.transneft.ru>
2023-03-22 11:36:26 +0300

    Тестирование работы модели-представления

--- CV19/ViewModels/MainWindowViewModel.cs
+++ CV19/ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CV19.ViewModels.Base;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,19 @@ using System.Threading.Tasks;
 
 namespace CV19.ViewModels
 {
-    internal class MainWindowViewModel
+    internal class MainWindowViewModel : ViewModel 
     {
+        #region Заголовок окна
+        private string _title = "Анализ статиcтики CV19";
+		/// <summary>
+		/// Заголовок окна
+		/// </summary>
+		public string Title
+		{
+			get => _title;
+			set => Set(ref _title, value);
+		}
+        #endregion
+
     }
 }
