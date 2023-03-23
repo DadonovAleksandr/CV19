1319a20370620ede703c68a33796487d47de47cc
DadonovAD <DadonovAD@tvv.transneft.ru>
2023-03-23 13:39:17 +0300

    Базовый класс команды

--- CV19/ViewModels/MainWindowViewModel.cs
+++ CV19/ViewModels/MainWindowViewModel.cs
@@ -21,5 +21,17 @@ namespace CV19.ViewModels
 		}
         #endregion
 
+        #region Статус
+        private string _status = "Готов!";
+        /// <summary>
+        /// Статус программы
+        /// </summary>
+        public string Status
+        {
+            get => _status;
+            set => Set(ref _status, value);
+        }
+        #endregion
+
     }
 }
