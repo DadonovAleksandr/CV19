b704ad1b019435d6e1e213402792d52a99f906ee
DadonovAD <DadonovAD@tvv.transneft.ru>
2023-03-23 13:53:42 +0300

    Мелкие правки

--- CV19/Infrastructure/Commands/LambdaCommand.cs
+++ CV19/Infrastructure/Commands/LambdaCommand.cs
@@ -1,12 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CV19.Infrastructure.Commands.Base;
+using System;
 
-namespace CV19.Infrastructure.Commands
+namespace CV19.Infrastructure.Commands;
+
+internal class LambdaCommand : Command
 {
-    internal class LambdaCommand
+    private readonly Action<object> _execute;
+    private readonly Func<object, bool> _canExecute;
+
+    public LambdaCommand(Action<object> Execute, Func<object, bool> CanExecute = null)
     {
+        _execute = Execute ?? throw new ArgumentNullException(nameof(Execute));
+        _canExecute = CanExecute;
     }
+
+    public override bool CanExecute(object? parameter) => _canExecute?.Invoke(parameter) ?? true;
+    
+    public override void Execute(object? parameter) => _execute?.Invoke(parameter);
+    
 }
+
