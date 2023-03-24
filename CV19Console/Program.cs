b8f78538986c860bdeadc08e66aa9c17484e1677
DadonovAD <DadonovAD@tvv.transneft.ru>
2023-03-24 17:15:25 +0300

    Запрос к удаленному серверу для получения сырых данных

--- CV19Console/Program.cs
+++ CV19Console/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using System.Net;
+
+const string dataUrl = 
+    @"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
+
+HttpClient client = new HttpClient();
+var responce= client.GetAsync(dataUrl).Result;
+var csvStr = responce.Content.ReadAsStringAsync().Result;
+Console.WriteLine(csvStr);
+Console.ReadLine();
