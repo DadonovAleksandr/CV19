dcf882a9103895761e7ae33291729ef5af7b6440
DadonovAD <DadonovAD@tvv.transneft.ru>
2023-03-28 11:04:52 +0300

    Добавляем модели

---
+++ CV19/Models/DataPoint.cs
@@ -0,0 +1,8 @@
+﻿namespace CV19.Models;
+
+internal class DataPoint
+{
+    public double XValue { get; set; }  
+    public double YValue { get; set; }
+}
+
--- CV19/ViewModels/MainWindowViewModel.cs
+++ CV19/ViewModels/MainWindowViewModel.cs
@@ -1,53 +1,74 @@
 ﻿using CV19.Infrastructure.Commands;
+using CV19.Models;
 using CV19.ViewModels.Base;
+using System;
+using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Input;
 
-namespace CV19.ViewModels
+namespace CV19.ViewModels;
+
+internal class MainWindowViewModel : ViewModel 
 {
-    internal class MainWindowViewModel : ViewModel 
+    private IEnumerable<DataPoint> _testDataPoints;
+
+    public IEnumerable<DataPoint> TestDataPoints
     {
-        public MainWindowViewModel()
+        get => _testDataPoints;
+        set => Set(ref _testDataPoints, value);
+    }
+
+    public MainWindowViewModel()
+    {
+        var data_points = new List<DataPoint>((int)(360/0.1));    
+        for(var x = 0d; x <= 360; x += 0.1)
         {
-            #region Команды
-            CloseApplicationCommand = new LambdaCommand(OnCloseApplicationCommandExecuted, CanCloseApplicationCommandExecute);
-            #endregion
+            const double to_rad = Math.PI / 180;
+            var y = Math.Sin(x * to_rad);
+            data_points.Add(new DataPoint() { XValue = x, YValue = y });
         }
+
+        TestDataPoints = data_points;
 
         #region Команды
-
-        public ICommand CloseApplicationCommand { get; }
-        private void OnCloseApplicationCommandExecuted(object p) 
-        {
-            Application.Current.Shutdown();
-        }
-        private bool CanCloseApplicationCommandExecute(object p) => true;
-
+        CloseApplicationCommand = new LambdaCommand(OnCloseApplicationCommandExecuted, CanCloseApplicationCommandExecute);
         #endregion
-
-        #region Заголовок окна
-        private string _title = "Анализ статиcтики CV19";
-		/// <summary>
-		/// Заголовок окна
-		/// </summary>
-		public string Title
-		{
-			get => _title;
-			set => Set(ref _title, value);
-		}
-        #endregion
-
-        #region Статус
-        private string _status = "Готов!";
-        /// <summary>
-        /// Статус программы
-        /// </summary>
-        public string Status
-        {
-            get => _status;
-            set => Set(ref _status, value);
-        }
-        #endregion
-
     }
+
+    #region Команды
+
+    public ICommand CloseApplicationCommand { get; }
+    private void OnCloseApplicationCommandExecuted(object p) 
+    {
+        Application.Current.Shutdown();
+    }
+    private bool CanCloseApplicationCommandExecute(object p) => true;
+
+    #endregion
+
+    #region Заголовок окна
+    private string _title = "Анализ статиcтики CV19";
+	/// <summary>
+	/// Заголовок окна
+	/// </summary>
+	public string Title
+	{
+		get => _title;
+		set => Set(ref _title, value);
+	}
+    #endregion
+
+    #region Статус
+    private string _status = "Готов!";
+    /// <summary>
+    /// Статус программы
+    /// </summary>
+    public string Status
+    {
+        get => _status;
+        set => Set(ref _status, value);
+    }
+    #endregion
+
+
 }
