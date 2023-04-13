ad8ed5b911578a1896484202d1d35f42dacb5462
DadonovAD <DadonovAD@tvv.transneft.ru>
2023-04-13 10:21:50 +0300

    Мультиконвертор

---
+++ CV19/Infrastructure/Convertors/MultiConvertor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace CV19.Infrastructure.Convertors
+{
+    internal abstract class MultiConvertor : IMultiValueConverter
+    {
+        public abstract object Convert(object[] values, Type t, object p, CultureInfo c);
+        
+        public virtual object[] ConvertBack(object value, Type[] t, object p, CultureInfo c) =>
+            throw new NotSupportedException("Обратное преобразование не поддерживается");
+    }
+}
