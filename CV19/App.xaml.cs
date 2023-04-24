d462a83af3d561c3a735a05b85c688b4f7f40466
DadonovAD <DadonovAD@tvv.transneft.ru>
2023-04-24 16:23:58 +0300

    Запуск и остановка хоста при старте и выходе

--- CV19/App.xaml.cs
+++ CV19/App.xaml.cs
@@ -6,22 +6,35 @@ using System;
 using System.Linq;
 using System.Windows;
 
-namespace CV19
+namespace CV19;
+
+public partial class App : Application 
 {
-    public partial class App : Application 
+    public static bool IsDesignMode {  get; private set; } = true;
+    private static IHost _host;
+    public static IHost Host => _host ??= Program.CreateHostBuilder(Environment.GetCommandLineArgs()).Build();
+
+    internal static void ConfigureServices(HostBuilderContext host, IServiceCollection services)
     {
-        public static bool IsDesignMode {  get; private set; } = true;
+        services.AddSingleton<DataService>();
+        services.AddSingleton<CountriesStatisticViewModel>();
+    }
 
-        internal static void ConfigureServices(HostBuilderContext host, IServiceCollection services)
-        {
-            services.AddSingleton<DataService>();
-            services.AddSingleton<CountriesStatisticViewModel>();
-        }
+    protected override async void OnStartup(StartupEventArgs e)
+    {
+        IsDesignMode = false;
+        var host = Host;
+        base.OnStartup(e);
+        host.StartAsync().ConfigureAwait(false);
+    }
 
-        protected override void OnStartup(StartupEventArgs e)
-        {
-            IsDesignMode = false;
-            base.OnStartup(e);
-        }
+    protected override async void OnExit(ExitEventArgs e)
+    {
+        base.OnExit(e);
+
+        var host = Host;
+        host.StopAsync().ConfigureAwait(false);
+        host.Dispose();
+        _host = null;
     }
 }
