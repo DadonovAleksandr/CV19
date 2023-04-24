f98184a50ed89c3088b85cd9430866f04eca2ed0
DadonovAD <DadonovAD@tvv.transneft.ru>
2023-04-24 15:13:14 +0300

    Мелкие правки

--- CV19/Infrastructure/Convertors/MultiConvertor.cs
+++ CV19/Infrastructure/Convertors/MultiConvertor.cs
@@ -3,12 +3,11 @@ using System.Globalization;
 using System.Windows.Data;
 
 namespace CV19.Infrastructure.Convertors
+
+internal abstract class MultiConvertor : IMultiValueConverter
 {
-    internal abstract class MultiConvertor : IMultiValueConverter
-    {
-        public abstract object Convert(object[] values, Type t, object p, CultureInfo c);
+    public abstract object Convert(object[] values, Type t, object p, CultureInfo c);
         
-        public virtual object[] ConvertBack(object value, Type[] t, object p, CultureInfo c) =>
-            throw new NotSupportedException("Обратное преобразование не поддерживается");
-    }
+    public virtual object[] ConvertBack(object value, Type[] t, object p, CultureInfo c) =>
+        throw new NotSupportedException("Обратное преобразование не поддерживается");
 }
