e5185404027500c1dcd8273c53d6e3c84a7e4cbf
DadonovAD <DadonovAD@tvv.transneft.ru>
2023-05-19 11:26:28 +0300

    Заготовка асинхронного сервера

--- CV19.Web/WebServer.cs
+++ CV19.Web/WebServer.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +10,58 @@ namespace CV19.Web;
 
 internal class WebServer
 {
+    //private TcpListener _listener = new TcpListener(new IPEndPoint(IPAddress.Any, 8080));
+    private HttpListener _listener;
+    private readonly int _port;
+    private bool _enabled;
+    private readonly object _lock = new object();
+
+    public int Port => _port;
+    public bool Enabled
+    {
+        get => _enabled;
+        set
+        {
+            if(value) Start();
+            else Stop();
+        }
+    }
+
+    public WebServer(int port) => _port = port;
+    
+
+    public void Start()
+    {
+        if (_enabled) return;
+        lock(_lock) 
+        {
+            if (_enabled) return;
+
+            _listener = new HttpListener();
+            _listener.Prefixes.Add($"http://*:{_port}");
+            _listener.Prefixes.Add($"http://+:{_port}");
+            _enabled = true;
+        }
+        Listen();
+    }
+
+    public void Stop() 
+    {
+        if(!_enabled) return;
+
+        lock(_lock) 
+        {
+            if (!_enabled) return;
+
+            _listener = null;
+            _enabled = false;
+        }
+    }
+
+    private void Listen()
+    {
+
+    }
+
 
 }
