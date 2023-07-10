26b11756bbcd8ea609b286ed4f7f9877de121480
DadonovAD <DadonovAD@tvv.transneft.ru>
2023-07-10 15:55:47 +0300

    Системное меню в иконке в верхнем левом углу окна

---
+++ CV19/Infrastructure/Extensions/WindowExtensions.cs
@@ -0,0 +1,25 @@
+﻿using CV19.Infrastructure.Extensions;
+using System.Runtime.InteropServices;
+using System.Windows.Interop;
+
+namespace System.Windows;
+
+internal static class WindowExtensions
+{
+    private const string user32 = "user32.dll";
+    
+    [DllImport(user32, CharSet = CharSet.Auto)]
+    private static extern IntPtr SendMessage(IntPtr hWnd, uint Msg, IntPtr wParam, IntPtr lParam);
+
+    public static IntPtr SendMessage(this Window window, WM Msg, SC wParam, IntPtr lParam = default)
+    {
+        return SendMessage(window.GetWindowHandle(), (uint)Msg, (IntPtr)wParam, lParam == default ? (IntPtr)' ' : lParam);
+    }
+
+    public static IntPtr GetWindowHandle(this Window window) 
+    {
+        var heper = new WindowInteropHelper(window);
+        return heper.Handle;
+    }
+
+}
