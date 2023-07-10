b794a8c26ba9d353f01bc0dcddbff36124cf6fb4
DadonovAD <DadonovAD@tvv.transneft.ru>
2023-07-10 14:54:20 +0300

    Методы-расширения для поиска элемент а среди родительских

---
+++ CV19/Infrastructure/Behaviors/WindowTitleBarBehavior.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Xaml.Behaviors;
+using System.Windows;
+
+namespace CV19.Infrastructure.Behaviors;
+
+internal class WindowTitleBarBehavior : Behavior<UIElement>
+{
+    private Window _window;
+
+    protected override void OnAttached()
+    {
+        _window = AssociatedObject as Window ?? AssociatedObject;
+    }
+
+    protected override void OnDetaching()
+    {
+        base.OnDetaching();
+    }
+
+
+
+
+}
